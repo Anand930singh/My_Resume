191a0ef18a4962cc7deab312ba5d060c957194ad
Anand930singh <anand2002king@gmail.com>
2023-01-15 19:36:49 +0530

    mongoose connect

--- server/db/config.js
+++ server/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/resume")
+mongoose.connect("mongodb+srv://Anand:Atlas2002@mern-versel.bhv99xc.mongodb.net/?retryWrites=true&w=majority")

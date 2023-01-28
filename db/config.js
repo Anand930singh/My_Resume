858db48dda3373465bf03fc671dc84a5710f7ce7
Anand930singh <99159646+Anand930singh@users.noreply.github.com>
2023-01-28 18:04:49 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://Anand:Atlas2002@mern-versel.bhv99xc.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Anand:Atlas2002@usersdata.ubcpwk3.mongodb.net/?retryWrites=true&w=majority")
 // mongodb+srv://Anand:Atlas2002@usersdata.ubcpwk3.mongodb.net/?retryWrites=true&w=majority
 // mongodb+srv://Anand:Atlas2002@mern-versel.bhv99xc.mongodb.net/?retryWrites=true&w=majority
 
